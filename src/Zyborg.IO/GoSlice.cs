f9d37a49b6c5da8a31be70b82800272cb929f406
Eugene Bekker <ebekker@users.noreply.github.com>
2018-01-16 17:43:35 -0500

    Adding helpers comparable with their Go equivalents

--- src/Zyborg.IO/GoSlice.cs
+++ src/Zyborg.IO/GoSlice.cs
@@ -198,6 +198,26 @@ namespace Zyborg.IO
 
     public static class SliceExtensions
     {
+        public static slice<char> AsCharSlice(this string s)
+        {
+            return new slice<char>(s.ToCharArray());
+        }
+
+        public static slice<byte> AsByteSlice(this string s)
+        {
+            return new slice<byte>(System.Text.Encoding.UTF8.GetBytes(s));
+        }
+
+        public static string AsString(this slice<char> s)
+        {
+            return new string(s.ToArray());
+        }
+
+        public static string AsString(this slice<byte> s)
+        {
+            return System.Text.Encoding.UTF8.GetString(s.ToArray());
+        }
+
         public static slice<T> slice<T>(this T[] array, int lower = 0, int upper = int.MinValue)
         {
             return new slice<T>(array, lower, upper);
@@ -223,5 +243,88 @@ namespace Zyborg.IO
                 index++;
             }
         }
+
+        /// EncodeRune writes into p (which must be large enough) the UTF-8 encoding of the rune.
+        /// It returns the number of bytes written.
+        public static int EncodeRune(this slice<byte> p, char r)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(new char[] { r });
+            switch (bytes.Length)
+            {
+                case 1:
+                    p[0] = bytes[0];
+                    return 1;
+                case 2:
+                    p[0] = bytes[1];
+                    p[1] = bytes[0];
+                    return 2;
+                case 3:
+                    p[0] = bytes[2];
+                    p[1] = bytes[1];
+                    p[2] = bytes[0];
+                    return 3;
+                case 4:
+                    p[0] = bytes[3];
+                    p[1] = bytes[2];
+                    p[2] = bytes[1];
+                    p[3] = bytes[0];
+                    return 4;
+                default:
+                    throw new Exception("failed to encode UTF8 character to slice"
+                            + $" -- unexpected byte length [{bytes.Length}]");
+            }
+        }
+
+        /// DecodeRune unpacks the first UTF-8 encoding in p and returns the rune and
+        /// its width in bytes. If p is empty it returns (RuneError, 0). Otherwise, if
+        /// the encoding is invalid, it returns (RuneError, 1). Both are impossible
+        /// results for correct, non-empty UTF-8.
+        ///
+        /// An encoding is invalid if it is incorrect UTF-8, encodes a rune that is
+        /// out of range, or is not the shortest possible UTF-8 encoding for the
+        /// value. No other validation is performed.
+        public static (char r, int size) DecodeRune(this slice<byte> p)
+        {
+            var (r, size) = NStack.Utf8.DecodeRune(p.ToArray());
+            return ((char)r, size);
+
+            // n := len(p)
+            // if n < 1 {
+            //     return RuneError, 0
+            // }
+            // p0 := p[0]
+            // x := first[p0]
+            // if x >= as {
+            //     // The following code simulates an additional check for x == xx and
+            //     // handling the ASCII and invalid cases accordingly. This mask-and-or
+            //     // approach prevents an additional branch.
+            //     mask := rune(x) << 31 >> 31 // Create 0x0000 or 0xFFFF.
+            //     return rune(p[0])&^mask | RuneError&mask, 1
+            // }
+            // sz := x & 7
+            // accept := acceptRanges[x>>4]
+            // if n < int(sz) {
+            //     return RuneError, 1
+            // }
+            // b1 := p[1]
+            // if b1 < accept.lo || accept.hi < b1 {
+            //     return RuneError, 1
+            // }
+            // if sz == 2 {
+            //     return rune(p0&mask2)<<6 | rune(b1&maskx), 2
+            // }
+            // b2 := p[2]
+            // if b2 < locb || hicb < b2 {
+            //     return RuneError, 1
+            // }
+            // if sz == 3 {
+            //     return rune(p0&mask3)<<12 | rune(b1&maskx)<<6 | rune(b2&maskx), 3
+            // }
+            // b3 := p[3]
+            // if b3 < locb || hicb < b3 {
+            //     return RuneError, 1
+            // }
+            // return rune(p0&mask4)<<18 | rune(b1&maskx)<<12 | rune(b2&maskx)<<6 | rune(b3&maskx), 4
+        }        
     }
 }
